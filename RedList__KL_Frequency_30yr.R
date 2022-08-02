6a669a2101e8954389fa9fff6697f34d38811e20
arunvgh <110190693+arunvgh@users.noreply.github.com>
2022-08-02 21:04:43 +0530

    Add files via upload

--- RedList__KL_Frequency_30yr.R
+++ RedList__KL_Frequency_30yr.R
@@ -1,7 +1,12 @@
-###################### Required ##########################
+##################### Required ##########################
 library (dplyr)  # For Pipes %>%
 library (lubridate) # For Date Function
 library (tidyr) # For pivot_wider Function
+library (plyr) # For join Function
+library(conflicted)
+conflict_prefer("arrange", "dplyr")
+conflict_prefer("summarize", "dplyr")
+conflict_prefer("mutate", "dplyr")
 #############################################################
 # Clear the Console and the Environment
 rm(list=ls()) 
@@ -18,9 +23,9 @@ ebdfile <- paste0("ebd_IN-KL_rel",month.abb[CurMonth],"-",CurYear)
 # List the interested columns
 
 preimp <- c(
-              "TAXONOMIC.ORDER","CATEGORY","SCIENTIFIC.NAME","COMMON.NAME","OBSERVATION.COUNT","STATE","APPROVED",
-              "OBSERVATION.DATE","GROUP.IDENTIFIER","SAMPLING.EVENT.IDENTIFIER"
-            )
+  "TAXONOMIC.ORDER","CATEGORY","SCIENTIFIC.NAME","COMMON.NAME","OBSERVATION.COUNT","STATE","APPROVED",
+  "OBSERVATION.DATE","GROUP.IDENTIFIER","SAMPLING.EVENT.IDENTIFIER"
+)
 
 #Incase the unzip is not done, uncomment this line
 # if (unzip)
@@ -30,14 +35,14 @@ preimp <- c(
 
 # Read the header plus first row
 nms <- read.delim( 
-                   paste0 (ebdfile,".txt"),
-                   nrows = 1, 
-                   sep = '\t', 
-                   header = T, 
-                   quote = "", 
-                   stringsAsFactors = F, 
-                   na.strings = c ("", " ",NA)
-                  )
+  paste0 (ebdfile,".txt"),
+  nrows = 1, 
+  sep = '\t', 
+  header = T, 
+  quote = "", 
+  stringsAsFactors = F, 
+  na.strings = c ("", " ",NA)
+)
 
 nms <- names(nms)
 nms [!(nms %in% preimp)] <- "NULL"
@@ -62,85 +67,93 @@ save(ebd, file = "KL_RedListData_30YR.RData")
 
 # Remove unapproved species (typically Exotics)
 ebd <- ebd %>%
-            filter (APPROVED == 1)
+  filter (APPROVED == 1)
 
 # Remove STATE and APPROVED column and downsize data
 ebd <- ebd %>% 
-            select (
-              CATEGORY, TAXONOMIC.ORDER, SCIENTIFIC.NAME,COMMON.NAME,OBSERVATION.COUNT,OBSERVATION.DATE,
-              GROUP.IDENTIFIER,SAMPLING.EVENT.IDENTIFIER)
+  select (
+    CATEGORY, TAXONOMIC.ORDER, SCIENTIFIC.NAME,COMMON.NAME,OBSERVATION.COUNT,OBSERVATION.DATE,
+    GROUP.IDENTIFIER,SAMPLING.EVENT.IDENTIFIER)
 
 ebd = ebd %>%
-            mutate (
-              OBSERVATION.DATE = as.Date(OBSERVATION.DATE),
-              YEAR  = year(OBSERVATION.DATE)
-            )
+  mutate (
+    OBSERVATION.DATE = as.Date(OBSERVATION.DATE),
+    YEAR  = year(OBSERVATION.DATE)
+  )
 
 # Remove OBSERVATION.DATE column and downsize data
 ebd <- ebd %>% 
-              select (
-                CATEGORY, TAXONOMIC.ORDER, SCIENTIFIC.NAME,COMMON.NAME,OBSERVATION.COUNT,YEAR,
-                GROUP.IDENTIFIER,SAMPLING.EVENT.IDENTIFIER)
+  select (
+    CATEGORY, TAXONOMIC.ORDER, SCIENTIFIC.NAME,COMMON.NAME,OBSERVATION.COUNT,YEAR,
+    GROUP.IDENTIFIER,SAMPLING.EVENT.IDENTIFIER)
 
 ebd <- ebd %>%
-              filter (YEAR >= CurYear-30 & YEAR < CurYear)
+  filter (YEAR >= CurYear-30 & YEAR < CurYear)
 
 # Add GROUP.ID for finding unique lists to help remove duplicate checklist
 ebd <- ebd %>% 
-              mutate (
-                      GROUP.ID = ifelse (
-                                          is.na(GROUP.IDENTIFIER), 
-                                          SAMPLING.EVENT.IDENTIFIER,
-                                          GROUP.IDENTIFIER
-                                        )
-                    )
+  mutate (
+    GROUP.ID = ifelse (
+      is.na(GROUP.IDENTIFIER), 
+      SAMPLING.EVENT.IDENTIFIER,
+      GROUP.IDENTIFIER
+    )
+  )
 
 # Remove SAMPLING.EVENT.IDENTIFIER & GROUP.IDENTIFIER as its not needed anymore          
 ebd <- ebd %>% 
-              distinct (CATEGORY,TAXONOMIC.ORDER,SCIENTIFIC.NAME,COMMON.NAME,OBSERVATION.COUNT,YEAR,GROUP.ID)
+  distinct (CATEGORY,TAXONOMIC.ORDER,SCIENTIFIC.NAME,COMMON.NAME,OBSERVATION.COUNT,YEAR,GROUP.ID)
 
 ebd <- ebd %>%
-              filter (CATEGORY %in% c('species','issf','domestic'))
-  
-# Remove CATEGORY as its not needed anymore          
+  filter (CATEGORY %in% c('species','issf','domestic'))
+
+# Remove GROUP.ID as its not needed anymore          
 ebd <- ebd %>% 
-              distinct (TAXONOMIC.ORDER,SCIENTIFIC.NAME,COMMON.NAME,OBSERVATION.COUNT,YEAR,GROUP.ID)
+  distinct (CATEGORY,TAXONOMIC.ORDER,SCIENTIFIC.NAME,COMMON.NAME,OBSERVATION.COUNT,YEAR)
+
+# To be used later
+ebd_d <- ebd
+
+# Remove CATEGORY, TAXONOMIC.ORDER, as its not needed anymore          
+ebd <- ebd %>% 
+  distinct (SCIENTIFIC.NAME,COMMON.NAME,OBSERVATION.COUNT,YEAR)
 
 ebd_f <- ebd
-
 ebd_f$OBSERVATION.COUNT <- 1
-  
-data_1 <- ebd_f %>%
-                  group_by(TAXONOMIC.ORDER,SCIENTIFIC.NAME,COMMON.NAME,YEAR) %>%
-                  summarize(count = sum(OBSERVATION.COUNT)) %>%
-                  arrange(YEAR,TAXONOMIC.ORDER,SCIENTIFIC.NAME,COMMON.NAME,desc(count))
 
-data_2 <- pivot_wider(data_1, names_from = YEAR, values_from = count)
-data_2 <- data_2[order(data_2$TAXONOMIC.ORDER),]
+data_1 <- ebd_f %>%
+                group_by(SCIENTIFIC.NAME,COMMON.NAME,YEAR) %>%
+                summarize(count = sum(OBSERVATION.COUNT)) %>%
+                arrange(YEAR, SCIENTIFIC.NAME,COMMON.NAME,desc(count))
+
+data_2 <- pivot_wider(data_1, names_from = YEAR, values_from = count )
 data_2[is.na(data_2)] <- 0
 
 data_3 <- data_2 %>%
-                  mutate(COUNT = NA)
-data_3 = subset(data_3, select = -c(TAXONOMIC.ORDER) )
-
-unique_rows <- !duplicated(data_3[c("SCIENTIFIC.NAME","COMMON.NAME")])
-
-unique.data_3 <- data_3[unique_rows,]
-
-data_4 <- data_3 %>% 
-  group_by(SCIENTIFIC.NAME,COMMON.NAME) %>% 
-  summarise(across(everything(), sum), .groups = 'drop')
-
-
-
-# data_3$COUNT <- rowSums(!is.na(data_3[3:32]))
+  group_by(SCIENTIFIC.NAME,COMMON.NAME) %>%
+  summarise_all(sum) %>%
+  mutate(COUNT = NA)
 
 data_3$COUNT <- rowSums((data_3[3:32])>0)
 
+# Generate Data Set for Scientific Names and Taxonomic Order
+ebd_d1 <- ebd_d %>% 
+                distinct (TAXONOMIC.ORDER, SCIENTIFIC.NAME,CATEGORY)
+ebd_d1 <- ebd_d1[order(ebd_d1$TAXONOMIC.ORDER),]
+ebd_d1 <- ebd_d1 %>%
+                group_by(SCIENTIFIC.NAME) %>%
+                mutate(rank = rank(SCIENTIFIC.NAME, ties.method = "first"))
+ebd_d1 <- ebd_d1[!(ebd_d1$rank==2 | ebd_d1$rank==3),]
+
+# Final Data with Taxonomic Order
+data_4 <- join(data_3, ebd_d1, by="SCIENTIFIC.NAME")
+data_4 = subset(data_4, select = -c(CATEGORY,rank) )
+data_4 <- data_4 %>%
+                select(TAXONOMIC.ORDER, everything())
+data_4 <- data_4[order(data_4$TAXONOMIC.ORDER),]
+
 # Save the dataframe in RDS file
-saveRDS(data_3,"KL_Count_YearWise.RDS")
+saveRDS(data_4,"KL_Count_YearWise.RDS")
 
 # Write to csv file
-write.csv(data_3,"D:/ArunV/R Programming Ebird/KL_Count_YearWise.csv", row.names = FALSE)
-
-
+write.csv(data_4,"D:/ArunV/R Programming Ebird/KL_Count_YearWise.csv", row.names = FALSE)
